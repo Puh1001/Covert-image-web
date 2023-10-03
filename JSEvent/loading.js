9ddb020b5cd4e54d4246d47ce9833a4a987df76f
DO DANG HOAN <121910478+DODANGHOANCNTT2K15@users.noreply.github.com>
2023-10-03 21:04:38 +0700

    DODANGHOAN_UI_LOADING_03102023

---
+++ JSEvent/loading.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    var element = document.querySelector('.loading');
+    if (element) {
+        element.style.height = '0vh';
+        document.querySelector('.loading>img').style.display = 'none';
+    }
+}, 2000);
