03dc5f9675c89fbc533fc844ce28fac899ca2e99
DO DANG HOAN <121910478+DODANGHOANCNTT2K15@users.noreply.github.com>
2023-10-27 18:25:09 +0700

    fix_2_reponsive_tablet

--- JSEvent/button_main_tool.js
+++ JSEvent/button_main_tool.js
@@ -11,7 +11,25 @@ button_taskbar_phone.addEventListener("click", function() {
 
 var button_taskbar_phone_exit = document.querySelector(".tool_1 img")
 button_taskbar_phone_exit.addEventListener("click", function(){
-    mainEdit.style.transform  = "scaleX(0)";
-    button_taskbar_phone.style.display = "block";
-    mainEdit.style.pointerEvents = "auto";
+    if(window.innerWidth <= 500){
+        mainEdit.style.transform  = "scaleX(0)";
+        button_taskbar_phone.style.display = "block";
+        mainEdit.style.pointerEvents = "auto";
+    }else if(window.innerWidth > 500 && window.innerWidth <= 1000){
+        button_taskbar_Tablet.style.display = "block";
+        mainEdit.style.transform  = "scaleX(0)";
+        // mainEdit.style.display  = "none";
+    }
+});
+// máy tính bảng
+
+var button_taskbar_Tablet = document.querySelector(".taskbar_Tablet")
+button_taskbar_Tablet.addEventListener("click", function(){
+    mainEdit.style.display  = "block";
+    mainEdit.style.position = "absolute";
+    mainEdit.style.transform  = "scaleX(1)";
+    mainEdit.style.height = "100vh";
+    mainEdit.style.width = "55vw";
+    button_taskbar_phone_exit.style.pointerEvents = "initial";
+    button_taskbar_Tablet.style.display = "none";
 });
