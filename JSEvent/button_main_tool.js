ab6ef7434774b06afc2cf5088311b2ffedc42e43
DO DANG HOAN <121910478+DODANGHOANCNTT2K15@users.noreply.github.com>
2023-10-27 17:36:07 +0700

    fix_2_reponsive_phone

--- JSEvent/button_main_tool.js
+++ JSEvent/button_main_tool.js
@@ -6,10 +6,12 @@ button_taskbar_phone.addEventListener("click", function() {
     mainEdit.style.transform  = "scaleX(1)";
     button_taskbar_phone.style.display = "none";
     mainEdit.style.height = "100vh";
+    button_taskbar_phone_exit.style.pointerEvents = "initial";
 });
 
 var button_taskbar_phone_exit = document.querySelector(".tool_1 img")
 button_taskbar_phone_exit.addEventListener("click", function(){
     mainEdit.style.transform  = "scaleX(0)";
     button_taskbar_phone.style.display = "block";
+    mainEdit.style.pointerEvents = "auto";
 });
