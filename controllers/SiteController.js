7fca94e0bc12ea01da48450edc07ebde8ded1386
Puh <hoangvn2phu@gmail.com>
2023-11-09 15:34:04 +0700

    add 404 dowload

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -9,10 +9,18 @@ class SiteController {
     download(req, res) {
         // const file = path.join(__dirname, "..", 'output', `image.png`);
         const file = getImgPath();
+        // Kiểm tra xem người dùng đã nhập hình ảnh hay chưa
+        if (!file) {
+            // Nếu không có hình ảnh, chuyển hướng hoặc xử lý khác tại đây
+            res.sendFile(path.join(__dirname, '..','HTML','erro.html'));
+            return;
+        }
+
         res.download(file, (err) => {
             if (err) {
                 // Xử lý lỗi ở đây
                 console.log(err);
+                res.get(path.join(__dirname, '..','HTML','erro.html'));
             } else {
                 //xóa ảnh sau khi tải về
                 deleteImage(file);
